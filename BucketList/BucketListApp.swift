880df48e9b5917d03bfe8ee3658f6b057816f387
yerdi <yerdi.isman@gmail.com>
2023-01-21 22:47:19 +0600

    first commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Yerdaulet Ismanaliyev on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
